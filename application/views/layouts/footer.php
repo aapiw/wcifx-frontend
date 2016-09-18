aaae4298ff1867cbe381a4e679affee1e193b3ce
yasfi <yasfi.mail@gmail.com>
2016-09-18 15:59:41 +0700

    update the footer

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,52 +1,100 @@
 <div class="container-fluid footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				<img src="../../dist/images/payments.jpg" alt="payments">
+			<div class="col-md-12 sec-1">
+				<img src="../../dist/images/payments.png" alt="payments">
 			</div>
-			<div class="col-md-12">
-				<div class="col-md-2">
+			<div class="col-md-12 sec-2">
+				<div class="col-sm-2 col-xs-6">
 					<a href="#x">Open Acount</a>
 					<a href="#x">Deposite Account</a>
 				</div>
-				<div class="col-md-2">
+				<div class="col-sm-2 col-xs-6">
 					<a href="#x">Download terminal</a>
 					<a href="#x">Open account</a>
 				</div>
-				<div class="col-md-2">
+				<div class="col-sm-2 col-xs-6">
 					<a href="#x">Get Bonus</a>
 					<a href="#x">Participate in composition</a>
 				</div>
-				<div class="col-md-2">
+				<div class="col-sm-2 col-xs-6">
 					<a href="#x">Subcribe to signals</a>
 					<a href="#x">Becam signal provider</a>
 				</div>
-				<div class="col-md-2">
+				<div class="col-sm-2 col-xs-6">
 					<a href="#x">Invest</a>
 					<a href="">Became partner</a>
 				</div>
 			</div>
-			<div class="col-md-12">
+			<div class="col-md-12 sec-3">
 				<div class="col-md-3">
-					<img src="../../dist/images/g-play.jpg" alt="">
+					<div class="play" style="background:url('../../dist/images/g-play.png') no-repeat"></div>
 				</div>
 				<div class="col-md-3">
-					<img src="../../dist/images/g-play.jpg" alt="">
+					<div class="app-store" style="background:url('../../dist/images/g-play.png') no-repeat"></div>
 				</div>
 				<div class="col-md-4 social-footer">
 					<small>Follow us</small>
-					<div clas="1"><a href="#x"><i class="fa fa-twitter"></i></a></div>
-			        <div clas="2"><a href="#x"><i class="fa fa-instagram"></i></a></div>
-			        <div clas="3"><a href="#x"><i class="fa fa-google-plus"></i></a></div>
-			        <div clas="4"><a href="#x"><i class="fa fa-youtube"></i></a></div>
+					<a href="#x"><div class="a"><i class="fa fa-facebook"></i></div></a>
+			        <a href="#x"><div class="b"><i class="fa fa-twitter"></i></div></a>
+			        <a href="#x"><div class="c"><i class="fa fa-youtube"></i></div></a>
+			        <a href="#x"><div class="d"><i class="fa fa-linkedin"></i></div></a>
+			        <a href="#x"><div class="e"><i class="fa fa-path"></i></div></a>
+			        <a href="#x"><div class="f"><i class="fa fa-instagram"></i></div></a>
+			        <a href="#x"><div class="g"><i class="fa fa-google-plus"></i></div></a>
+			        <a href="#x"><div class="h"><i class="fa fa-wordpress"></i></div></a>
 				</div>
 				<div class="col-md-2">
-					<a href="#x" class="btn btn-brand btn-mistake">.i.fa.fa-Found a mistake?</a>
+					<a href="#x" class="btn btn-brand btn-green"><i class="fa fa-Found"></i>Found a mistake?</a>
 				</div>
 			</div>
-			<div class="col-md-12"></div>
-			<div class="col-md-12">
-				<span class="text-left">&copy; 2016 MFX <span class="xtra-small"> << </span> Broker <span class="xtra-small"> >> </span></span> <span class="text-center">Information postted website is of general nature and provided for information only </span>
+			<div class="col-md-12 sec-4">
+				<div class="col-md-2 col-sm-3 col-xs-6">
+					<p class="title">Trade with us</p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses</p>
+				</div>
+				<div class="col-md-2 col-sm-3 col-xs-6">
+					<p class="title">Trade Invest</p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+				</div>
+				<div class="col-md-2 col-sm-3 col-xs-6">
+					<p class="title">Participate</p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+				</div>
+				<div class="col-md-2 col-sm-3 col-xs-6">
+					<p class="title">Become partner</p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+				</div>
+				<div class="col-md-2 col-sm-3 col-xs-6">
+					<p class="title">About company</p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+				</div>
+				<div class="col-md-2 col-sm-3 col-xs-6">
+					<p class="title">Trade eith us</p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+					<p>Bonuses </p>
+				</div>
+			</div>
+			<div class="col-md-12 sec-5">
+				<span class="text-left">&copy; 2016 MFX <span class="xtra-small"> <i class="fa fa-angle-double-left"></i> </span>
+				 Broker <span class="xtra-small"> <i class="fa fa-angle-double-right"></i></span></span> <p class="text-center">Information postted website is of general nature and provided for information only </p>
 			</div>
 		</div>
 	</div>
